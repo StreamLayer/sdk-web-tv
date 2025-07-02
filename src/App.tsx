0ed9c80a7ab9ca3232763d016b2fc881a32c7561
renat <renat0014@gmail.com>
2025-07-02 10:33:27 +0400

    fix: update release

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,20 @@ import { cx } from '@emotion/css'
 import '@streamlayer/react/style.css'
 import { EVENT_ID, SDK_KEY, PRODUCTION } from './config'
 
-export type IMode = 'side-panel' | 'l-bar' | 'overlay' | 'off'
-export type ITheme = 'light' | 'dark' | 'tgl'
-
 const plugins = new Set([anonymous])
 
 function App() {
   return (
     <div className={cx('app-container')}>
-      <StreamLayerProvider themeMode={'light'} plugins={plugins as any} withAdNotification sdkKey={SDK_KEY} theme="custom-theme" production={PRODUCTION} event={EVENT_ID}>
+      <StreamLayerProvider
+        themeMode={'light'}
+        plugins={plugins as any}
+        autoEnable
+        withAd
+        sdkKey={SDK_KEY}
+        production={PRODUCTION}
+        event={EVENT_ID}
+      >
         <StreamLayerSDKTv persistent>
           <video
             src="https://storage.googleapis.com/cdn.streamlayer.io/assets/sdk-web/Own%20The%20Game%201080p%20RF18.mp4"
