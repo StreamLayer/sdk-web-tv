99f2899c0300b0942c9c4891c1583e565bac1773
renat <renat0014@gmail.com>
2025-12-15 11:39:42 +0400

    feat: update docs, remove project code. Now this repo only for deploy docs

--- setupTests.js
+++
@@ -1,6 +0,0 @@
-import "@testing-library/jest-dom";
-import { TextEncoder, TextDecoder } from 'util';
-import * as dotenv from 'dotenv';
-
-dotenv.config({ path: './.env' });
-Object.assign(global, { TextDecoder, TextEncoder });
--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import EnvironmentPlugin from "vite-plugin-environment"
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '',
-  plugins: [react(), EnvironmentPlugin("all")],
-})
